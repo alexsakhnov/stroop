7842ced4e6e9bf898048a2d225fdd0e96584182c
Aleksey Sakhnov <alexsakhnov@gmail.com>
2022-07-02 21:12:39 +0300

    Updates

---
+++ precache-manifest.15c8fe3558687eb7af7f0d148af2877d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1d4e7226e52d991af879",
+    "url": "/stroop/static/css/main.a04358e9.chunk.css"
+  },
+  {
+    "revision": "1d4e7226e52d991af879",
+    "url": "/stroop/static/js/main.1d4e7226.chunk.js"
+  },
+  {
+    "revision": "c972895fac6c9db70f84",
+    "url": "/stroop/static/js/1.c972895f.chunk.js"
+  },
+  {
+    "revision": "c8cc87d74b787f4b353f",
+    "url": "/stroop/static/js/2.c8cc87d7.chunk.js"
+  },
+  {
+    "revision": "1ddc3f20074282b7723d",
+    "url": "/stroop/static/js/runtime~main.1ddc3f20.js"
+  },
+  {
+    "revision": "0fc372a907f2d7bf13408ab72507a22e",
+    "url": "/stroop/index.html"
+  }
+];
